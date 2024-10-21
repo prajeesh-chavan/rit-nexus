890e8a9f9b7a8067a2419692e4d947be6d9d0558
Prajeesh Chavan <prajeeshchavan@gmail.com>
2024-10-21 22:57:19 +0530

    changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -88,7 +88,7 @@ const HomePage = () => {
             Featured Blogs
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {posts.map((post) => (
+            {posts.slice(0, 3).map((post) => (
               <BlogCard key={post._id} post={post} />
             ))}
           </div>
